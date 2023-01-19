acc6bc1bece9417648a2d361cd84bed57c960c87
Sophie Dong <sophiedong2007@gmail.com>
2023-01-19 10:19:29 -0500

    d

--- vite-project/js/characters.js
+++ vite-project/js/characters.js
@@ -11,7 +11,7 @@ async function getData(URL) {
         document.getElementById("api-response").insertAdjacentHTML(
           "afterbegin",
           `<div class="character-card">
-                <img class="img" src="https://static.wikia.nocookie.net/gensin-impact/images/1/15/Character_Hu_Tao_Card.png/revision/latest?cb=20220725204937" alt="Hu Tao">                
+                <img class="img" src="" alt="Character Icon">                
                 <h3 class= "character-name">${character.name}</h3>
                 <p class="character-weapon">${character.weapon}</p>
                 <p class="character-vision">${character.vision}</p>
