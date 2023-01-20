32649c6ba04677d451202cec6506525cac10e1e5
Sophie Dong <sophiedong2007@gmail.com>
2023-01-19 23:07:18 -0500

    ..

---
+++ vite-project/js/_dom.js
@@ -0,0 +1,23 @@
+const DOMSelectors = {
+  all: document.getElementById("all"),
+  female: document.getElementById("female"),
+  male: document.getElementById("male"),
+  pyro: document.getElementById("pyro"),
+  hydro: document.getElementById("hydro"),
+  cyro: document.getElementById("cyro"),
+  anemo: document.getElementById("anemo"),
+  electro: document.getElementById("electro"),
+  dendro: document.getElementById("dendro"),
+  geo: document.getElementById("geo"),
+  bow: document.getElementById("bow"),
+  catalyst: document.getElementById("catalyst"),
+  claymore: document.getElementById("claymore"),
+  sword: document.getElementById("sword"),
+  polearm: document.getElementById("polearm"),
+  mondstadt: document.getElementById("mondstadt"),
+  liyue: document.getElementById("liyue"),
+  inazuma: document.getElementById("inazuma"),
+  sumeru: document.getElementById("sumeru"),
+  text: document.querySelector("text"),
+};
+export { DOMSelectors };
--- vite-project/js/characters.js
+++ vite-project/js/characters.js
@@ -1,13 +1,50 @@
-import "../styles/style.css";
+import { DOMSelectors } from "./_dom";
 
 const URL = "https://api.genshin.dev/characters/all";
+
+const query = async function () {
+  try {
+    const response = await fetch(URL);
+    const data = await response.json();
+    const types = data.types;
+    const characters = types.characters;
+  } catch (error) {
+    console.log(error);
+    alert("An error occured.");
+  }
+};
+
+function displayBooks() {
+  characters.books.forEach((character) => {
+    DOMSelectors.popularBooks.insertAdjacentHTML(
+      "beforeend",
+      `<div class="popular-list">
+              <img
+              src="${book.book_image}"
+              alt="Book Cover"
+              class="book-cover">
+              <div class="book-info">
+                  <h1 class="book-title">${book.title}</h1>
+                  <p class="book-author">${book.contributor}</p>
+                  <p class="book-description">
+                      ${book.description}
+                  </p>
+                  <div class="buy-btn">
+                      <a class="shop-btn" href="${book.amazon_product_url}">Buy Now</a></div>
+                  </div>
+              </div>
+          </div>`
+    );
+  });
+}
+displayBooks();
 
 async function getData(URL) {
   try {
     const response = await fetch(URL);
     const data = await response.json();
     function displayCharacters() {
-      data.characters.forEach((character) => {
+      data.entity.forEach((character) => {
         document.getElementById("api-response").insertAdjacentHTML(
           "afterbegin",
           `<div class="character-card">
