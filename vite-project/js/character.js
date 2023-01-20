8216896e2b50f1a263b93ba0491651af91996aeb
Sophie Dong <sophiedong2007@gmail.com>
2023-01-20 10:19:13 -0500

    a

--- vite-project/js/character.js
+++ vite-project/js/character.js
@@ -1,29 +1,33 @@
 import { DOMSelectors } from "./_dom";
 
+const URL = "https://api.genshin.dev/characters/all";
+const imgUrl1 = "https://api.genshin.dev/characters/";
+const imgUrl2 = "/card";
+
 const query = async function () {
   try {
     const response = await fetch(URL);
     const data = await response.json();
-    const types = data.types;
-    const characters = types.characters;
+    function displayCharacters() {
+      data.forEach((ppl) => {
+        let test = ppl.name;
+        let test2 = imgUrl1 + test + imgUrl2;
+        DOMSelectors.character.insertAdjacentHTML(
+          "beforeend",
+          `<div class="character-card">
+          <img src= ${test2} alt="Character Icon">               
+                    <h3 class= "character-name">${ppl.name}</h3>
+                    <p class="character-weapon">${ppl.weapon}</p>
+                    <p class="character-vision">${ppl.vision}</p>
+                    <p class="character-nation">${ppl.nation}</p>
+                </div>`
+        );
+      });
+    }
+    displayCharacters();
   } catch (error) {
     console.log(error);
     alert("An error occured.");
   }
 };
-
-function displayCharacters() {
-  characters.ppl.forEach((ppl) => {
-    DOMSelectors.characters.insertAdjacentHTML(
-      "beforeend",
-      `<div class="character-card">
-                <img class="img" src="" alt="Character Icon">                
-                <h3 class= "character-name">${ppl.name}</h3>
-                <p class="character-weapon">${ppl.weapon}</p>
-                <p class="character-vision">${ppl.vision}</p>
-                <p class="character-nation">${ppl.nation}</p>
-            </div>`
-    );
-  });
-}
-displayCharacters();
+query();
